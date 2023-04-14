34be74bf40ee240ac38863321e1e36a4a81a77b4
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-14 04:38:51 +0100

    export data in the CSV format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,49 +5,27 @@ Python script to export data in the CSV format.
 """
 
 
-import requests
-import csv
-import sys
+if __name__ == '__main__':
+    import csv
+    import requests
+    import sys
 
-EMPLOYEE_ID = sys.argv[1]
+    t = sys.argv[1]
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + t)
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + t)
 
-# make the API call to get employee information
-response = requests.get(
-    f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}")
-employee_data = response.json()
-employee_name = employee_data["name"]
+    employee = response.json()
+    todos = todos.json()
+    id = employee['id']
+    name = employee['username']
 
-# make the API call to get TODO list for the employee
-response = requests.get(
-    f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYEE_ID}")
-todo_list = response.json()
-
-# open a CSV file for writing
-filename = f"{EMPLOYEE_ID}.csv"
-with open(filename, mode='w', newline='') as csv_file:
-    fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-    writer.writeheader()
-
-    # write each task to the CSV file
-    for task in todo_list:
-        writer.writerow({
-            'USER_ID': EMPLOYEE_ID,
-            'USERNAME': employee_name,
-            'TASK_COMPLETED_STATUS': str(task['completed']),
-            'TASK_TITLE': task['title']
-        })
-
-# count completed tasks
-completed_tasks = [todo for todo in todo_list if todo["completed"]]
-num_completed_tasks = len(completed_tasks)
-total_num_tasks = len(todo_list)
-
-print(
-    f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
-
-
-# print completed tasks
-for task in completed_tasks:
-    print(f"\t{task['title']}")
+    with open(f'{t}.csv', 'w') as file:
+        for i in todos:
+            Ta = i['completed']
+            TASK_TITLE = i['title']
+            TASK_COMPLETED_STATUS = i['completed']
+            TASK_TITLE = i['title']
+            file.write(
+                f"\"{id}\",\"{name}\",\"{Ta}\",\"{TASK_TITLE}\"\n")
