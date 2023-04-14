452060c9afbdbcd28eb623417c2878b9f34086a4
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-14 04:51:28 +0100

    to export data in the JSON format

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -8,13 +8,11 @@ import requests
 import sys
 EMPLOYEE_ID = sys.argv[1]
 
-# make the API call to get employee information
 response = requests.get(
     f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}")
 employee_data = response.json()
 employee_name = employee_data["username"]
 
-# make the API call to get TODO list for the employee
 response = requests.get(
     f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYEE_ID}")
 todo_list = response.json()
@@ -27,12 +25,10 @@ for task in todo_list:
 
 json_data = {EMPLOYEE_ID: tasks}
 
-# write the JSON data to a file
 filename = f"{EMPLOYEE_ID}.json"
 with open(filename, mode='w') as json_file:
     json.dump(json_data, json_file, indent=4)
 
-# count completed tasks
 completed_tasks = [todo for todo in todo_list if todo["completed"]]
 num_completed_tasks = len(completed_tasks)
 total_num_tasks = len(todo_list)
@@ -40,6 +36,5 @@ total_num_tasks = len(todo_list)
 print(
     f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
 
-# print completed tasks
 for task in completed_tasks:
     print(f"\t{task['title']}")
