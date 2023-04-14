874f5df547ba761bc67617cd21088cb3a3c8c6a2
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-14 04:32:33 +0100

    export data in the CSV format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,12 +9,14 @@ import sys
 EMPLOYEE_ID = sys.argv[1]
 
 # make the API call to get employee information
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}")
+response = requests.get(
+    f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}")
 employee_data = response.json()
 employee_name = employee_data["name"]
 
 # make the API call to get TODO list for the employee
-response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYEE_ID}")
+response = requests.get(
+    f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYEE_ID}")
 todo_list = response.json()
 
 # open a CSV file for writing
@@ -39,7 +41,9 @@ completed_tasks = [todo for todo in todo_list if todo["completed"]]
 num_completed_tasks = len(completed_tasks)
 total_num_tasks = len(todo_list)
 
-print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
+print(
+    f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
+
 
 # print completed tasks
 for task in completed_tasks:
