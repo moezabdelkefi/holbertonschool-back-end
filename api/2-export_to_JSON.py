43c28aa3c9b164f2d14576ab8071a96d9cab6685
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-14 04:51:01 +0100

    to export data in the JSON format

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -5,21 +5,24 @@ Python script to export data in the JSON format.
 """
 import json
 import requests
-import sys  
+import sys
 EMPLOYEE_ID = sys.argv[1]
 
 # make the API call to get employee information
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}")
+response = requests.get(
+    f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}")
 employee_data = response.json()
 employee_name = employee_data["username"]
 
 # make the API call to get TODO list for the employee
-response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYEE_ID}")
+response = requests.get(
+    f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYEE_ID}")
 todo_list = response.json()
 
 tasks = []
 for task in todo_list:
-    task_dict = {"task": task['title'], "completed": task['completed'], "username": employee_name}
+    task_dict = {
+        "task": task['title'], "completed": task['completed'], "username": employee_name}
     tasks.append(task_dict)
 
 json_data = {EMPLOYEE_ID: tasks}
@@ -34,7 +37,8 @@ completed_tasks = [todo for todo in todo_list if todo["completed"]]
 num_completed_tasks = len(completed_tasks)
 total_num_tasks = len(todo_list)
 
-print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
+print(
+    f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
 
 # print completed tasks
 for task in completed_tasks:
